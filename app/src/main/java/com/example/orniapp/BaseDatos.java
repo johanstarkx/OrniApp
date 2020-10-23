940a5a7655c3c90c4f1a08ae95e4bcf0bc43a8cd
johan <johanstarkx@gmail.com>
2020-10-22 22:22:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/orniapp/BaseDatos.java
@@ -0,0 +1,4 @@
+package com.example.orniapp;
+
+public class BaseDatos {
+}
---
+++ app/src/main/java/com/example/orniapp/CatologoOrni.java
@@ -0,0 +1,14 @@
+package com.example.orniapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatologoOrni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.catalogoorni);
+    }
+}
